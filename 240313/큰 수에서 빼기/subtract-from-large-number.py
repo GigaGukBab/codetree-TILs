99f616f490e736b159fe7455f424a9f99a375d40
Han Jin Woo <128575512+GigaGukBab@users.noreply.github.com>
2024-03-13 22:22:58 +0900

    Title: 큰 수에서 빼기, Time: 131ms, Memory: 73MB, Status: Passed - CodeTree

--- 240313/큰 수에서 빼기/subtract-from-large-number.py
+++ 240313/큰 수에서 빼기/subtract-from-large-number.py
@@ -4,9 +4,7 @@ a, b = in_arr.split()[0:2]
 
 a, b = int(a), int(b)
 
-if a>b:
-    print(a-b)
-elif a<b:
-    print(b-a)
+if a < b:
+    print(b - a)
 else:
-    print("both are the same number")
+    print(a - b)
