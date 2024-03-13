69f48b708e7731edfe2dc0eac592cd6fdd758f2f
Han Jin Woo <128575512+GigaGukBab@users.noreply.github.com>
2024-03-13 22:11:49 +0900

    Title: 합과 평균의 차, Time: 117ms, Memory: 73MB, Status: Passed - CodeTree

--- 240313/합과 평균의 차/sub-of-average-and-sum.py
+++ 240313/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,9 +1,13 @@
 in_arr = input()
 
-    in_arr = in_arr.split()
+in_arr = in_arr.split()
 
-    in_arr = [int(items) for items in in_arr]
+in_arr = [int(items) for items in in_arr]
 
-    num_sum = sum(in_arr)
-    num_avg = sum(in_avg) // len(in_arr)
-    num_diff = num_sum - num_avg
+num_sum = sum(in_arr)
+num_avg = sum(in_arr) // len(in_arr)
+num_diff = num_sum - num_avg
+
+print(num_sum)
+print(num_avg)
+print(num_diff)
