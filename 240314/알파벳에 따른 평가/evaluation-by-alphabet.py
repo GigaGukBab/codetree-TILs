8eeb73a698dd170bb66cd174be3a36e5bb42e7ae
Han Jin Woo <128575512+GigaGukBab@users.noreply.github.com>
2024-03-14 20:11:18 +0900

    Title: 알파벳에 따른 평가, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240314/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+alphabet_capital = input()
+
+if alphabet_capital == 'S':
+    print("Superior")
+elif alphabet_capital == 'A':
+    print("Excellent")
+elif alphabet_capital == 'B':
+    print("Good")
+elif alphabet_capital == 'C':
+    print("Usually")
+elif alphabet_capital == 'D':
+    print("Effort")
+else :
+    print("Failure")
