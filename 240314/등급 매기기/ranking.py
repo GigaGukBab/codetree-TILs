30c533f737aee2bc7ca2241f3d4c90a89b5265ae
Han Jin Woo <128575512+GigaGukBab@users.noreply.github.com>
2024-03-14 20:08:14 +0900

    Title: 등급 매기기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240314/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+grade = int(input())
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+elif grade < 60:
+    print("F")
