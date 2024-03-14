dcfae264b925626e7f166c2cdb9371c5f5aaa747
Han Jin Woo <128575512+GigaGukBab@users.noreply.github.com>
2024-03-14 20:05:01 +0900

    Title: 출석 부르기, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240314/출석 부르기/calling-attendance.py
@@ -0,0 +1,10 @@
+attendance_number = int(input())
+
+if attendance_number == 1:
+    print("John")
+elif attendance_number == 2:
+    print("Tom")
+elif attendance_number == 3:
+    print("Paul")
+else:
+    print("Vacancy")
