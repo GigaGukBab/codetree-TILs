3f607f06982013875c975d26e3f1ab02c6b3e270
Jinwoo Han <128575512+GigaGukBab@users.noreply.github.com>
2024-03-18 20:27:26 +0900

    Title: 세 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240318/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240318/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -2,19 +2,11 @@ num_arr = input()
 num_arr = list(map(int, num_arr.split()))
 a, b, c = num_arr[0], num_arr[1], num_arr[2]
 
-if a > b:
-    min_val = b
-else:
+if a < b and a < c:
     min_val = a
-
-if b > c:
-    min_val = c
-else:
+elif b < a and b < c:
     min_val = b
-
-if c > b:
-    min_val = b
-else:
+elif c < a and c < b:
     min_val = c
 
 print(min_val)
