e78133cbe414d7bc019ed7c0eebf67deb5613801
Jinwoo Han <128575512+GigaGukBab@users.noreply.github.com>
2024-05-17 19:58:16 +0900

    Title: 두 사람, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240517/두 사람/two-person.py
+++ 240517/두 사람/two-person.py
@@ -10,7 +10,8 @@ second_person_age = int(second_person_input_list[0])
 second_person_sex = second_person_input_list[1]
 
 # 조건문을 통해 결과 출력
-if ((first_person_age >=19 and first_person_sex == "M") or (second_person_age >= 19 and first_person_sex == "M")):
+if ((first_person_age >=19 and first_person_sex == "M") or 
+    (second_person_age >= 19 and first_person_sex == "M")):
     print(1)
 else:
     print(0)
