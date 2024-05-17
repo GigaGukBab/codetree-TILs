aba88908e2af98c2675d29d58dbf52cfeb6b1a2f
Jinwoo Han <128575512+GigaGukBab@users.noreply.github.com>
2024-05-17 20:00:59 +0900

    Title: 두 사람, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240517/두 사람/two-person.py
+++ 240517/두 사람/two-person.py
@@ -11,7 +11,7 @@ second_person_sex = second_person_input_list[1]
 
 # 조건문을 통해 결과 출력
 if ((first_person_age >=19 and first_person_sex == "M") or 
-    (second_person_age >= 19 and first_person_sex == "M")):
+    (second_person_age >= 19 and second_person_sex == "M")):
     print(1)
 else:
     print(0)
